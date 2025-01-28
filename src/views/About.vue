b967a4d9d9a6562c6d008692e08545d177419069
Kennu156 <kenmarttipaju@gmail.com>
2025-01-28 23:47:53 +0200

    CV, self-analysis and about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,67 +1,199 @@
 <script setup>
+import { ref } from 'vue';
 import Footer from '../components/Footer.vue';
 import Navigation from '../components/navigation.vue';
 
+const isDropdownOpen = ref(false);
+const isSecondDropdownOpen = ref(false);
 </script>
-
 
 <template>
     <div class="bg-slate-900 pt-7">
         <Navigation/>
     </div>
     <section class="min-h-screen bg-slate-900 text-emerald-400 font-satoshi px-6 py-12 flex items-center pt-24">
-  <div class="w-full max-w-4xl mx-auto">
-    <h1 class="text-4xl md:text-6xl font-bold text-center mb-8">About Me</h1>
-    <p class="text-lg md:text-xl text-center text-emerald-200 mb-8">
-      Hi, I'm Ken-Martti Paju. A passionate web designer and developer with a focus on creating
-      beautiful and functional websites that make an impact.
-    </p>
-    
-    <div class="flex justify-center mb-12">
-      <img src="./img/anne2.jpg" alt="" class="rounded-lg shadow-lg w-full max-w-md md:max-w-lg hover:shadow-emerald-400/20 transition-shadow duration-300"/>
-    </div>
-    
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
-      <div class="p-6 bg-slate-800 rounded-lg border border-emerald-400/20 hover:border-emerald-400/40 transition-colors duration-300">
-        <h2 class="text-3xl font-semibold mb-4">My Mission</h2>
-        <p class="text-base text-emerald-200">
-          I strive to help businesses and individuals establish a strong online presence
-          through innovative design and user-friendly development. Whether it's a portfolio,
-          e-commerce site, or business page, I ensure that every project I work on reflects
-          the unique voice and goals of my clients.
-        </p>
-      </div>
-      
-      <div class="p-6 bg-slate-800 rounded-lg border border-emerald-400/20 hover:border-emerald-400/40 transition-colors duration-300">
-        <h2 class="text-3xl font-semibold mb-4">My Skills</h2>
-        <ul class="list-disc pl-5 text-emerald-200">
-          <li class="mb-2">Responsive Web Design</li>
-          <li class="mb-2">Front-End Development</li>
-          <li class="mb-2">Back-End Development</li>
-          <li class="mb-2">Content Creation</li>
-        </ul>
-      </div>
-    </div>
-    
-    <div class="text-center mt-12">
-      <a 
-        href="/contact" 
-        class="bg-emerald-400 text-slate-900 px-6 py-3 rounded-lg font-semibold hover:bg-emerald-300 transform hover:scale-[1.02] transition-all duration-300 inline-block"
-      >
-        Let's Collaborate
-      </a>
-    </div>
-  </div>
-</section>
-  <div class="pt-1 bg-slate-950"></div>
-  <Footer/>
+        <div class="w-full max-w-4xl mx-auto">
+            <h1 class="text-4xl md:text-6xl font-bold text-center mb-8">About Me</h1>
+            <p class="text-lg md:text-xl text-center text-emerald-200 mb-8">
+                Hi, I'm Ken-Martti Paju. A passionate web designer and developer with a focus on creating
+                beautiful and functional websites that make an impact.
+            </p>
+            
+            <div class="flex justify-center mb-12">
+                <img src="./img/anne2.jpg" alt="" class="rounded-lg shadow-lg w-full max-w-md md:max-w-lg hover:shadow-emerald-400/20 transition-shadow duration-300"/>
+            </div>
+            
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
+                <div class="p-6 bg-slate-800 rounded-lg border border-emerald-400/20 hover:border-emerald-400/40 transition-colors duration-300">
+                    <h2 class="text-3xl font-semibold mb-4">My Mission</h2>
+                    <p class="text-base text-emerald-200">
+                        I strive to help businesses and individuals establish a strong online presence
+                        through innovative design and user-friendly development. Whether it's a portfolio,
+                        e-commerce site, or business page, I ensure that every project I work on reflects
+                        the unique voice and goals of my clients.
+                    </p>
+                </div>
+                
+                <div class="p-6 bg-slate-800 rounded-lg border border-emerald-400/20 hover:border-emerald-400/40 transition-colors duration-300">
+                    <h2 class="text-3xl font-semibold mb-4">My Skills</h2>
+                    <ul class="list-disc pl-5 text-emerald-200">
+                        <li class="mb-2">Responsive Web Design</li>
+                        <li class="mb-2">Front-End Development</li>
+                        <li class="mb-2">Back-End Development</li>
+                        <li class="mb-2">Content Creation</li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="text-center mt-12">
+                <a 
+                    href="/contact" 
+                    class="bg-emerald-400 text-slate-900 px-6 py-3 rounded-lg font-semibold hover:bg-emerald-300 transform hover:scale-[1.02] transition-all duration-300 inline-block">
+                    Let's Collaborate
+                </a>
+            </div>
 
+            <div class="mt-16 bg-slate-800 p-6 rounded-lg shadow-lg border border-emerald-400/20">
+                <div 
+                    class="flex items-center justify-between cursor-pointer"
+                    @click="isDropdownOpen = !isDropdownOpen">
+                    <h2 class="text-3xl font-semibold mb-6">More About Me</h2>
+                    <svg 
+                        :class="isDropdownOpen ? 'transform rotate-180' : ''"
+                        class="w-6 h-6 text-emerald-400 transition-transform duration-300"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            stroke-width="2" 
+                            d="M19 9l-7 7-7-7"
+                        />
+                    </svg>
+                </div>
+
+                <div v-if="isDropdownOpen">
+                    <p class="text-lg text-emerald-200 mb-4">
+                        Outside of coding, I enjoy playing volleyball, which helps me improve my teamwork skills and stay physically active. I also love spending time in the sauna, a perfect way to relax and recharge.
+                    </p>
+                    <p class="text-lg text-emerald-200 mb-4">
+                        In my downtime, I indulge in computer games, a hobby that sharpens my logical thinking and reaction speed. Additionally, I enjoy social events and parties, creating unforgettable memories with friends.
+                    </p>
+                    <p class="text-lg text-emerald-200">
+                        A good sense of humor is one of my defining traits. I believe positivity and laughter are essential in overcoming challenges and making everyday life more enjoyable.
+                    </p>
+                </div>
+            </div>
+
+            <div class="mt-8 bg-slate-800 p-6 rounded-lg shadow-lg border border-emerald-400/20">
+                <div 
+                    class="flex items-center justify-between cursor-pointer"
+                    @click="isSecondDropdownOpen = !isSecondDropdownOpen">
+                    <h2 class="text-3xl font-semibold mb-6">CV</h2>
+                    <svg 
+                        :class="isSecondDropdownOpen ? 'transform rotate-180' : ''"
+                        class="w-6 h-6 text-emerald-400 transition-transform duration-300"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            stroke-width="2" 
+                            d="M19 9l-7 7-7-7"
+                        />
+                    </svg>
+                </div>
+
+                <div v-if="isSecondDropdownOpen">
+                    <div class="mb-4">
+                        <p class="text-lg text-emerald-200">
+                            <strong>Name:</strong>  KEN-MARTTI PAJU <br>
+                            <strong>Date of Birth:</strong>  18.01.2007 <br>
+                            <strong>Address:</strong> Kuressaare <br>
+                            <strong>Phone:</strong> +372 5191 9398 <br>
+                            <strong>Email:</strong> kenmarttipaju@gmail.com <br>
+                        </p>
+                    </div>
+                    <div class="mb-4">
+                        <h3 class="text-2xl font-semibold text-emerald-200">Education</h3>
+                        <p class="text-lg text-emerald-200 mt-1">
+                            2023 – Present: Kuressaare Ametikool, Junior Software Developer
+                        </p>
+                        <p class="text-lg text-emerald-200">
+                            2014 – 2023: Kuressaare Nooruse School
+                        </p>
+                    </div>
+
+                    <div class="mb-4">
+                        <h3 class="text-2xl font-semibold text-emerald-200">Additional</h3>
+                        <p class="text-lg text-emerald-200 mt-1">
+                            February 2024: Sillamäe Business Camp
+                        </p>
+                    </div>
+
+                    <div class="mb-4">
+                        <h3 class="text-2xl font-semibold text-emerald-200">Work Experience</h3>
+                        <p class="text-lg text-emerald-200 mt-2">
+                            May 2024 – June 2024: FuncSys OÜ (Intern)
+                        </p>
+                    </div>
+
+                    <div class="mb-4">
+                        <h3 class="text-2xl font-semibold text-emerald-200">Language Skills</h3>
+                        <p class="text-lg text-emerald-200 mt-1">
+                            Estonian - Native <br>
+                            English - Good understanding, good speaking skills, good writing skills
+                        </p>
+                    </div>
+
+                    <div class="mb-4">
+                        <h3 class="text-2xl font-semibold text-emerald-200">Skills</h3>
+                        <ul class="list-disc pl-5 text-emerald-200 mt-2">
+                            <li class="mb-2">Windows 10, XP, MS Office, Office365</li>
+                            <li class="mb-2">VSCode, Ubuntu</li>
+                        </ul>
+                    </div>
+
+                    <div class="mb-4">
+                        <h3 class="text-2xl font-semibold text-emerald-200">Hobbies</h3>
+                        <p class="text-lg text-emerald-200 mt-1">
+                            Volleyball, chess, gym, and disc-golf
+                        </p>
+                    </div>
+
+                    <div class="mb-4">
+                        <h3 class="text-2xl font-semibold text-emerald-200">Personal Traits</h3>
+                        <p class="text-lg text-emerald-200 mt-2">
+                          Humorous, adaptable, responsible, friendly, collaborative, calm, logical
+                        </p>
+                    </div>
+
+                    <div class="mb-4">
+                        <h3 class="text-2xl font-semibold text-emerald-200">References</h3>
+                        <ul class="list-disc pl-5 text-emerald-200 mt-1">
+                            <li class="mb-2"><strong>Annes Lõbus</strong>, CEO of FuncSys OÜ</li>
+                            <p>Phone: +372 5333 2895</p>
+                            <li class="mb-2 mt-3"><strong>Robby Saukštelis</strong>, classmate</li>
+                            <p>Phone: +372 583 7564</p>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <div class="pt-1 bg-slate-950"></div>
+    <Footer/>
 </template>
-
 
 <style scoped>
 .font-satoshi {
   font-family: 'Satoshi', sans-serif;
 }
 </style>
-
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,11 +1,7 @@
 <script setup>
 import Footer from '../components/Footer.vue';
 import Navigation from '../components/navigation.vue';
-
-
-
 </script>
-
 
 <template>
   <div class="bg-slate-900 pt-7">
@@ -18,9 +14,9 @@ import Navigation from '../components/navigation.vue';
       <p class="text-base md:text-xl opacity-75">My latest web design projects</p>
     </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6">
       <a href="https://github.com/robinmihkelson/Ametikooli-projekt-grupp-2-2023" target="_blank" class="group cursor-pointer relative overflow-hidden bg-slate-900 rounded-lg shadow-xl hover:scale-105 transform transition duration-300">
-        <img src="./img/image45.png" alt="Space" class="w-full h-full object-cover group-hover:brightness-75 transition duration-300"/>
+        <img src="./img/image45.png" alt="Space" class="w-full h-full object-cover group-hover:brightness-75 transition duration-300" />
         <div class="fixed inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
           <div class="text-center">
             <h2 class="text-2xl text-emerald-500">Saw X</h2>
@@ -28,9 +24,17 @@ import Navigation from '../components/navigation.vue';
           </div>
         </div>
       </a>
+      <div class="bg-slate-800 text-emerald-400 rounded-lg p-4 shadow-xl">
+        <h3 class="text-3xl font-bold mb-2">Self-Analysis</h3>
+        <p class="text-xl"><strong>Situation:</strong> IT project at Kuressaare Ametikool</p>
+        <p class="text-xl"><strong>Task:</strong> We had to create a webpage for a film.</p>
+        <p class="text-xl"><strong>Activities:</strong> In the team, I created the Figma design and wrote the HTML and CSS code.</p>
+        <p class="text-xl"><strong>Results:</strong> I gained a good understanding of Figma, HTML, and CSS by actually using them.</p>
+        <p class="text-xl"><strong>Reflection:</strong> This is a very effective way to learn professional skills. In my opinion, I did very well, considering that my skills at the time were quite limited. If I remember correctly, the teachers graded it positively. I don't think I would do anything differently. I have also applied these same skills in other projects and lessons. I feel like I still have a lot to learn.</p>
+      </div>
 
       <a href="https://github.com/mariliis01/disainimajaka-laenutus-rakendus" target="_blank" class="group cursor-pointer relative overflow-hidden bg-slate-900 rounded-lg shadow-xl hover:scale-105 transform transition duration-300">
-        <img src="./img/image34.png" alt="Nova" class="w-full h-full object-cover group-hover:brightness-75 transition duration-300"/>
+        <img src="./img/image34.png" alt="Nova" class="w-full h-full object-cover group-hover:brightness-75 transition duration-300" />
         <div class="fixed inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
           <div class="text-center">
             <h2 class="text-2xl text-black">Rental Application</h2>
@@ -38,9 +42,17 @@ import Navigation from '../components/navigation.vue';
           </div>
         </div>
       </a>
+      <div class="bg-slate-800 text-emerald-400 rounded-lg p-4 shadow-xl">
+        <h3 class="text-3xl font-bold mb-2">Self-Analysis</h3>
+        <p class="text-xl"><strong>Situation:</strong> IT project at Kuressaare Ametikool</p>
+        <p class="text-xl"><strong>Task:</strong> We had to create a camera rental application.</p>
+        <p class="text-xl"><strong>Activities:</strong> In the team, I helped with the Figma design and wrote the HTML, TailwindCSS and javascript code.</p>
+        <p class="text-xl"><strong>Results:</strong> As a result, I gained a good understanding of TailwindCSS and javascript by actually using them.</p>
+        <p class="text-xl"><strong>Reflection:</strong> This was a very effective way to increase my skills. In my opinion, I did okay, considering my skills were better by that point, but we couldn't finish due to database problems. I think I should've used javascript more. I have also applied these same skills in other projects and lessons.</p>
+      </div>
 
       <a href="https://github.com/JakobVapper/e-commerce-product-inventory-system" target="_blank" class="group cursor-pointer relative overflow-hidden bg-slate-900 rounded-lg shadow-xl hover:scale-105 transform transition duration-300">
-        <img src="./img/Grid.png" alt="Sonic" class="w-full h-full object-cover group-hover:brightness-75 transition duration-300"/>
+        <img src="./img/Grid.png" alt="Sonic" class="w-full h-full object-cover group-hover:brightness-75 transition duration-300" />
         <div class="fixed inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
           <div class="text-center">
             <h2 class="text-2xl text-emerald-400">E-com inventory system</h2>
@@ -48,13 +60,20 @@ import Navigation from '../components/navigation.vue';
           </div>
         </div>
       </a>
+      <div class="bg-slate-800 text-emerald-400 rounded-lg p-4 shadow-xl">
+        <h3 class="text-3xl font-bold mb-2">Self-Analysis</h3>
+        <p class="text-xl"><strong>Situation:</strong> IT project at Kuressaare Ametikool</p>
+        <p class="text-xl"><strong>Task:</strong> We had to create an e-com inventory system</p>
+        <p class="text-xl"><strong>Activities:</strong> In the team, I created the Figma design and learnt how to use AI with my projects</p>
+        <p class="text-xl"><strong>Results:</strong> I gained a good understanding of AI and it's uses by actually reading about it and using it the right way.</p>
+        <p class="text-xl"><strong>Reflection:</strong> This is a very effective way to learn professional skills. In my opinion, I could've done more, but my skills on the subject were limited. We finished it but we actually couldn't get the design working. Something I could've done differently was to talk to the team more. I have also applied these same skills in other projects and lessons. I feel like I still have a lot to learn.</p>
+      </div>
     </div>
   </section>
 
   <div class="bg-slate-950 pt-1"></div>
-  <Footer />
-  </template>
-  
+  <Footer/>
+</template>
 
 <style>
 .font-satoshi {
